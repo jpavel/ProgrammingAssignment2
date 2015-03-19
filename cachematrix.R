8b08b31b013b634f4d20bc138720f7041ce2f3b1
Pavel Jez <Pavel.Jez@cern.ch>
2015-03-19 14:27:10 +0100

    added few more comments to makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -10,7 +10,7 @@
 makeCacheMatrix <- function(x = matrix()) {
       inverse <-NULL # initialization of a variable that holds the inverse matrix
       set <- function(y) { 
-            # function to set a matrix from input y
+            ## function to set a matrix from input y to variable x
             
             # checking if the input is a square matrix. If not issue a warning
             if(!is.matrix(y)) warning("input object is not a matrix")
@@ -19,16 +19,18 @@ makeCacheMatrix <- function(x = matrix()) {
             x <<- y
             inverse <<- NULL
       }
-      get <- function() x
+      get <- function() x # returns matrix object stored in 'x'
       setinverse <- function(inv){
-            # check if inverse is a matrix and has the same dimensions than 'x'
+            ## function to set inverse matrix to 'x' to a variable 'inverse'
+            
+            # check if inverse is a matrix and has the same dimensions as 'x'
             if(!is.matrix(inv)) warning("inverse matrix is not a matrix")
             if(ncol(inv)!=ncol(x)||nrow(inv)!=nrow(x)) 
-                  warning("inverse has different dimensions than x")
+                  warning("input inverse matrix has different dimensions than x")
             
             inverse <<-inv    
       }
-      getinverse <- function() inverse
+      getinverse <- function() inverse #returns stored inverse matrix
       list(set = set, get = get, 
            setinverse = setinverse, getinverse = getinverse)
 }
